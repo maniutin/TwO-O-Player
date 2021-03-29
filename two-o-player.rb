dd978516da92d89d91e4b5784c225e6488ab0971
Nick Maniutin <maniutin@gmail.com>
2021-03-29 14:29:25 +0000

    new repo

---
+++ two-o-player.rb
@@ -0,0 +1,4 @@
+# nouns: player, turn, question
+# player: player name, starting HP, starting score. Methods: add score, remove HP
+# turn: question, answer, user I/O, player order loop
+# question: all the questions
