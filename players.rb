cc2327d78c742391ff3a6fe606a6964f9459d49a
Nick Maniutin <maniutin@gmail.com>
2021-03-29 14:46:28 +0000

    create a Player class

---
+++ players.rb
@@ -0,0 +1,16 @@
+class Player
+  attr_accessor :name, :score, :health
+
+  def initialize(name, score, health)
+    @name = name
+    @score = score
+    @health = health
+  end
+
+  def info
+    "#{name}: #{score} points #{health} HP"
+  end
+end
+
+player1 = Player.new('player1', '0/3', '3/3')
+puts player1.info
