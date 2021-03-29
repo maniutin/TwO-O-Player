c3dc86a30287bccdfcbbcea6346d273256edd9ab
Nick Maniutin <maniutin@gmail.com>
2021-03-29 18:41:15 +0000

    update main loop to complete the game

--- main.rb
+++ main.rb
@@ -9,54 +9,65 @@ def random_number
 end
 
 def new_game
-@question1 = Question.new(random_number, random_number)
-@question2 = Question.new(random_number, random_number)
+  @question1 = Question.new(random_number, random_number)
+  @question2 = Question.new(random_number, random_number)
+end
+
+def player1_round
+  puts "-----NEW TURN-----"
+
+  puts "#{@player1.name}: #{@question1.question}"
+  print "> "
+  answer = $stdin.gets.chomp.to_i
+
+  if answer == @question1.correct_answer
+    puts "#{@player1.name} answered #{answer}"
+    puts "Correct!"
+    @player1.score += 1
+  elsif answer != @question1.correct_answer
+    puts "#{@player1.name} answered #{answer}"
+    puts "Not correct at all!"
+    @player1.health -= 1
+  end
+  puts "#{@player1.info} vs #{@player2.info}"
+end
+
+def player2_round
+  puts "-----NEW TURN-----"
+
+  puts "#{@player2.name}: #{@question2.question}"
+  print "> "
+  answer = $stdin.gets.chomp.to_i
+
+  if answer == @question2.correct_answer
+    puts "#{@player2.name} answered #{answer}"
+    puts "Correct!"
+    @player2.score += 1
+  elsif answer != @question2.correct_answer
+    puts "#{@player2.name} answered #{answer}"
+    puts "Not correct at all!"
+    @player2.health -= 1
+  end
+  puts "#{@player1.info} vs #{@player2.info}"
 end
 
 new_game
 
 while @player1.health > 0 || @player2.health > 0
-  puts "#{@player1.name}: #{@question1.question}"
-  print "> "
-  answer = $stdin.gets.chomp.to_i
 
-  if answer === @question1.correct_answer
-    puts "#{@player1.name} answered #{answer}"
-    puts "Correct!"
-    @player1.score += 1
-  else
-    puts "#{@player1.name} answered #{answer}"
-    puts "Not correct at all!"
-    @player1.health -= 1
-  end
+  player1_round
+
+  player2_round
 
   new_game
 
-  puts "#{@player1.info} vs #{@player2.info}"
-  puts "-----NEW TURN-----"
-
-  puts "#{@player2.name}: #{@question1.question}"
-  print "> "
-  answer = $stdin.gets.chomp.to_i
-
-  if answer === @question1.correct_answer
-    puts "#{@player2.name} answered #{answer}"
-    puts "Correct!"
-    @player2.score += 1
-    puts @player2.info
-  else
-    puts "#{@player2.name} answered #{answer}"
-    puts "Not correct at all!"
-    @player2.health -= 1
-    puts @player2.info
-  end
-
-  puts "P1: #{@player2.info} vs P2: #{@player2.info}"
     if @player1.health == 0
       puts "#{@player2.name} wins with the score of #{@player2.score}"
-    elsif @player2.health == 0
+      break
+    end
+    if @player2.health == 0
       puts "#{@player1.name} wins with the score of #{@player1.score}"
-    puts "-----NEW TURN-----"
+      break
     end
 end
 
